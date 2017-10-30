22d99344bd1a37fdc5e83ff51ffc83dd3d3b538e
DavidRobertQuinn <davidrobertquinn@gmail.com>
2017-10-30 19:35:54 +0000

    Built scraper class with two methods and built some of the main function

--- scraper.py
+++ scraper.py
@@ -1,39 +1,40 @@
 """Script to scrape indeed.com and email me rlevant job openings"""
 
 import sys
-import urllib.request
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
 import bs4 as bs
+from collections import namedtuple
 
 def main():
     """Main entry point for the script."""
     scraper = Scraper("data", "cork")
     responses = scraper.get_indeed_data()
+    # print(responses[0])
     jobs = []
     for response in responses:
-        job_group = scraper.get_job_details_on_page()
+        job_group = scraper.get_job_details_on_page(response)
         jobs = jobs + job_group
-    pass
+    print(jobs[0])
 
 
 class Scraper():
     def __init__(self, keyword, location):
         self.location = location
         self.keyword = keyword
-        pass
 
 
     def get_indeed_data(self, days_ago_limit = 2,starting_page = 0, number_of_page_limit=10):
         "Returns list of responses of indeed.com for jobs in a a location and based on a keyword."
         indeed_url = "https://ie.indeed.com"
-        response = []
+        responses = []
         for i in range(starting_page, number_of_page_limit):
-            if i > number_of_page_limit:
-                url_of_job_page = "https://ie.indeed.com/jobs?q={0}&l={1}&start={2}&fromage={3}"
-                .format(self.keyword, self.location,i*10, days_ago_limit) 
+            if i < number_of_page_limit:
+                url_of_job_page = "https://ie.indeed.com/jobs?q={0}&l={1}&start={2}&fromage={3}".format(self.keyword, self.location,i*10, days_ago_limit) 
             else: 
                 break 
             try:
-                response = urlopen(url_of_job_pages).read()
+                response = urlopen(url_of_job_page).read()
             except HTTPError as e:
                 print('The server couldn\'t fulfill the request.')
                 print('Error code: ', e.code)
@@ -43,7 +44,7 @@ class Scraper():
             responses.append(response)
         return responses
 
-    def get_job_details_on_page(self ):
+    def get_job_details_on_page(self,response ):
         """Returns a list of namedtuples for each jobs by keyword in a defined location"""
         soup = bs.BeautifulSoup(response,"lxml")
         job_html_blocks = soup.find_all('div',{'data-tn-component' : "organicJob"})
@@ -57,28 +58,17 @@ class Scraper():
             link= job_attribute.get('href')
             job_info.append((title,companies[count], days_old[count], link))
         return [Job(*x) for x in job_info]
-                
-    def evaluate_job(self)
-
-    def download_html_of_jobs(self):
-        pass
-
-    def convert_html_to_text(self):
-        pass
 
 class JobSorter():
     def __init__(self):
         pass
 
-    def experience_level(self)
+    def experience_level(self):
         pass
-    def requirements(self)
+    def desired_skills(self):
         pass
-    def responsibilities(self)
+    def responsibilities(self):
         pass
-
-
-
 
 
 if __name__ == '__main__':
